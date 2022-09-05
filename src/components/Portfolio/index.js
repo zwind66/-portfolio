09a826872224322f1d85a0aab080dd3b838f780b
Shuai Zuo <szuo1@vols.utk.edu>
2022-09-04 23:07:39 -0500

    add portfolio

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Project from "../Project";
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Portfolio() {
+    const [pages] = useState([
+        {
+            name: "portfolio"
+        }
+    ]);
+    const [currentPage] = useState(pages[0]);
+    const [projects] = useState([
+        {
+            name: 'read-the-classic',
+            description: 'Node/Express/JS/Boostrap/SQL/ORM/MVC',
+            link: "https://read-the-calssics.herokuapp.com/",
+            repo: "https://github.com/corimest/read-the-classics"
+        },
+        {
+            name: 'run-buddy',
+            description: 'HTML/CSS',
+            link: "https://zwind66.github.io/run-buddy/",
+            repo: "https://github.com/zwind66/run-buddy"
+        },
+        {
+            name: 'what-s-for-tomorrow',
+            description: 'JavaScript/API/Bootstrap',
+            link: "https://zwind66.github.io/What-s-for-tomorrow/",
+            repo: "https://github.com/zwind66/What-s-for-tomorrow"
+        },
+        {
+            name: 'food-festival',
+            description: 'JS/Node/PWA',
+            link: "https://zwind66.github.io/food-festival/",
+            repo: "https://github.com/zwind66/food-festival"
+        },
+    
+        {
+            name: 'note-taker',
+            description: 'Node/Express/Bootstrap',
+            link: "https://note-taker-sz.herokuapp.com/",
+            repo: "https://github.com/zwind66/Note-Taker.git"
+        },
+        {
+            name: 'work-day-scheduler',
+            description: 'HTML/CSS/JavaScript',
+            link: "https://zwind66.github.io/Shuai-s-Assignment-5/",
+            repo: "https://github.com/zwind66/Shuai-s-Assignment-5"
+        },
+    ]);
+
+    return (
+        <section className="p-0">
+            <div className="container-fluid p-0">
+                <h1 className="text-center margin-top">{capitalizeFirstLetter(currentPage.name)}</h1>
+                <hr className="my-4" />
+                <div className="row no-gutters popup-gallery">
+                   
+                        {projects.map((project, idx) => (
+                            <Project
+                                project={project}
+                                key={"project" + idx}
+                            />
+                        ))}
+                    </div>
+                
+            </div>
+        </section>
+    );
+};
+
+export default Portfolio;
---
+++ src/components/Project/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { removeHyphensAndCapitalize } from '../../utils/helpers';
+
+
+function Project({ project }) {
+	const { name, repo, link, description } = project;
+
+	return (
+		<div className="col-lg-4 col-sm-6">
+		<div className="portfolio-box" key={name}>
+			<img
+				src={require(`../../assets/img/portfolio/${name}.jpg`)}
+				alt={removeHyphensAndCapitalize(name)}
+				className="img-fluid"
+			/>
+			<div className="portfolio-box-caption">
+				<div className="portfolio-box-caption-content">
+				<a href={repo} className="project-category text-faded" target="_blank">
+						<i className="fab fa-github"></i>
+					</a>
+					<br />
+					<a href={link} className="project-category text-faded" target="_blank">{removeHyphensAndCapitalize(name)}</a>{' '}
+					
+					<p className="project-name">{description}</p>
+				</div>
+			</div>
+		</div>
+		</div>
+	);
+}
+
+export default Project;
